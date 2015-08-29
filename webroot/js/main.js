4946ef298276fe9a72fd0cd83c9207e709e5644f
Mike Mac Pro <mike@saymikeo.com>
2015-08-29 13:31:58 -0400

    cleanup add comments

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -6,13 +6,14 @@ Form = {
     current_el: '.current',
     group_el: '.form-group',
     complete_el: '.complete',
+    tabbable_el: 'input.tabbable',
     init: function (form) {
         this.form = $(form);
         $(this.back_el+','+this.next_el).click(this.save.bind(this));
         $(this.back_el).click(this.back.bind(this));
         $(this.next_el).click(this.next.bind(this));
         $(this.complete_el).click(this.save_to_db.bind(this));
-        $('input.tabbable').on('keydown', this.handle_button.bind(this));
+        $(this.tabbable_el).on('keydown', this.handle_button.bind(this));
         this.update_nav();
     },
     /**
@@ -59,27 +60,53 @@ Form = {
     insert: function (field, value){
         $('[data-insert="'+field+'"]').html(value.trim());
     },
+    /**
+     * Move back in the form flow
+     * @param e
+     */
     back: function (e){
         e.preventDefault();
         var current = this.get_current_group();
         var last_page = this.get_prev_group();
         this.transition(current, last_page);
     },
+    /**
+     * Move forward in the form flow
+     * @param e
+     */
     next: function (e){
         e.preventDefault();
         var current = this.get_current_group();
         var next_page = this.get_next_group();
         this.transition(current, next_page);
     },
+    /**
+     * Get the currently displayed group of form elements
+     * @returns {*|jQuery|HTMLElement}
+     */
     get_current_group: function () {
         return $(this.current_el);
     },
+    /**
+     * Look ahead and get the next group of form elements
+     * Probably to display them
+     * @returns {*|jQuery}
+     */
     get_next_group: function () {
         return $(this.current_el).next(this.group_el);
     },
+    /**
+     * Look back and get the previous form elements
+     * @returns {*|jQuery}
+     */
     get_prev_group: function () {
         return $(this.current_el).prev(this.group_el);
     },
+    /**
+     * Transition the user from one form group to another
+     * @param current
+     * @param next
+     */
     transition: function (current, next){
         if(this.is_valid(current)) {
             $(current).removeClass('current')
@@ -92,6 +119,12 @@ Form = {
             }.bind(this))
         }
     },
+    /**
+     * Simple validation for empty fields
+     * TODO - make this more robust, Better insertion into the page.
+     * @param current
+     * @returns {boolean}
+     */
     is_valid: function (current) {
         error = false;
         $(current).find('input').each(function (i, el){
@@ -105,6 +138,10 @@ Form = {
         }
         return true;
     },
+    /**
+     * Hide or show next button based on other buttons on the page
+     * TODO - make this smarter. too much going on here.
+     */
     update_nav: function () {
         show = [];
 
@@ -136,6 +173,12 @@ Rating = {
     init: function () {
         $(this.rating_el).click(this.rate.bind(this));
     },
+    /**
+     * Rate a comment
+     * This will increment the users rating who wrote the comment
+     * and the comment's rating
+     * @param e
+     */
     rate: function (e) {
         e.preventDefault();
         var button = $(e.currentTarget);
@@ -152,6 +195,10 @@ Rating = {
             type: 'POST'
         });
     },
+    /**
+     * We have successully updated the rating, now let's update the view
+     * @param data
+     */
     success: function(data){
         if(data.id){
             this.comment.find(this.rating_el).addClass('rated');
@@ -159,6 +206,32 @@ Rating = {
             this.comment.find(this.user_points).html('+'+data.user.rating_count);
         }else{
             alert('Trek is off track. Please reload the page and try to rate again!');
+        }
+    }
+}
+
+Menu = {
+    toggleProfile: 'header #profile',
+    profileMenu: '.profile-menu',
+    init: function() {
+        $(this.toggleProfile).click(this.toggleMenu.bind(this));
+    },
+    toggleMenu: function(){
+        $(this.profileMenu).slideToggle("fast");
+    }
+}
+
+Scroll = {
+    scroll: 0,
+    init: function(){
+        $(window).scroll(this.scrolled.bind(this));
+    },
+    scrolled: function(){
+        this.scroll = $(window).scrollTop();
+        if (this.scroll > 0){
+            $('header').fadeOut('fast');
+        } else if(this.scroll === 0) {
+            $('header').fadeIn('fast');
         }
     }
 }
@@ -181,28 +254,4 @@ $(document).ready(function () {
     });
 });
 
-Menu = {
-    toggleProfile: 'header #profile',
-    profileMenu: '.profile-menu',
-    init: function() {
-        $(this.toggleProfile).click(this.toggleMenu.bind(this));
-    },
-    toggleMenu: function(){
-        $(this.profileMenu).slideToggle("fast");
-    }
-}
 
-Scroll = {
-    scroll: 0,
-    init: function(){
-        $(window).scroll(this.scrolled.bind(this));
-    },
-    scrolled: function(){
-        this.scroll = $(window).scrollTop();
-        if (this.scroll > 0){
-           $('header').fadeOut('fast'); 
-        } else if(this.scroll === 0) {
-            $('header').fadeIn('fast'); 
-        }
-    }
-}
